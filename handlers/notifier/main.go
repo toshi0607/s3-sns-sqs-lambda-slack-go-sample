6e156b967f143682a0c0b16e54ef65b2ae04bba0
toshi0607 <s.toshi0607@gmail.com>
2018-09-05 23:19:57 +0900

    :bug: SQS

--- handlers/notifier/main.go
+++ handlers/notifier/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, snsEvent events.SNSEvent) {
+	for _, record := range snsEvent.Records {
+		snsRecord := record.SNS
+
+		fmt.Printf("[%s %s] Message = %s \n", record.EventSource, snsRecord.Timestamp, snsRecord.Message)
+	}
 }
--- handlers/write_ext/main.go
+++ handlers/write_ext/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	for _, message := range sqsEvent.Records {
+		fmt.Printf("[[write_ext]] The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+	}
+
+	return nil
 }
