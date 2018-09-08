1b32c5c6a690f5abbeb77a2253cbac7667053fec
toshi0607 <s.toshi0607@gmail.com>
2018-09-08 15:25:31 +0900

    :tada: post Slack message

--- handlers/notifier/main.go
+++ handlers/notifier/main.go
@@ -1,21 +1,87 @@
 package main
 
 import (
-	"context"
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var client *Client
+
 func main() {
 	lambda.Start(handler)
 }
 
-func handler(ctx context.Context, snsEvent events.SNSEvent) {
-	for _, record := range snsEvent.Records {
-		snsRecord := record.SNS
+func init() {
+	client = NewClient(
+		config{
+			URL:       os.Getenv("WEBHOOK_URL"),
+			Channel:   os.Getenv("CHANNEL"),
+			Username:  os.Getenv("USERNAME"),
+			IconEmoji: os.Getenv("ICON"),
+		},
+	)
+}
 
-		fmt.Printf("[%s %s] Message = %s \n", record.EventSource, snsRecord.Timestamp, snsRecord.Message)
+func handler(snsEvent events.SNSEvent) error {
+	record := snsEvent.Records[0]
+	snsRecord := snsEvent.Records[0].SNS
+	fmt.Printf("[%s %s] Message = %s \n", record.EventSource, snsRecord.Timestamp, snsRecord.Message)
+
+	if err := client.PostMessage(snsRecord.Message); err != nil {
+		return err
 	}
+	return nil
+}
+
+type Client struct {
+	httpClient *http.Client
+	config     config
+}
+
+type config struct {
+	URL       string
+	Text      string `json:"text"`
+	Username  string `json:"username"`
+	IconEmoji string `json:"icon_emoji"`
+	Channel   string `json:"channel"`
+}
+
+func NewClient(c config) *Client {
+
+	return &Client{
+		httpClient: &http.Client{},
+		config:     c,
+	}
+}
+
+func (c Client) PostMessage(message string) error {
+	c.config.Text = message
+	p, _ := json.Marshal(c.config)
+
+	req, err := http.NewRequest(
+		"POST",
+		c.config.URL,
+		bytes.NewReader(p),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode >= 400 {
+		return fmt.Errorf("failed to send messages: %s", res.Status)
+	}
+
+	return nil
 }
---
+++ handlers/notifier/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/satori/go.uuid"
+)
+
+func TestHandler(t *testing.T) {
+
+	snsEvent := events.SNSEvent{
+		Records: []events.SNSEventRecord{
+			{
+				SNS: events.SNSEntity{
+					MessageID: uuid.Must(uuid.NewV4()).String(),
+					Message:   "message",
+				},
+			},
+		},
+	}
+	if err := handler(snsEvent); err != nil {
+		t.Error("error!")
+	}
+}
