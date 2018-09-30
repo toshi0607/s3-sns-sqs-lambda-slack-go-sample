7e88e75ab0b2aac2bff0abeb2efb4d7a8ea9cb33
toshi0607 <s.toshi0607@gmail.com>
2018-09-30 11:25:37 +0900

    :bug: fix parsing for event structure

--- handlers/write_file_name/main.go
+++ handlers/write_file_name/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/url"
 	"path/filepath"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/pkg/errors"
 )
+
+const s3PutEvent = "ObjectCreated:Put"
 
 func main() {
 	lambda.Start(handler)
@@ -18,24 +20,36 @@ func main() {
 
 func handler(sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
-		ext, err := getExtFromMessage(message)
+		ext, err := getFileNameFromMessage(message)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("extension of the file is %s", ext)
+		log.Printf("name of the file is %s", ext)
 	}
 
 	return nil
 }
 
-func getExtFromMessage(e events.SQSMessage) (string, error) {
-	var s3event events.S3Event
-	if err := json.Unmarshal([]byte(e.Body), &s3event); err != nil {
+func getFileNameFromMessage(e events.SQSMessage) (string, error) {
+	log.Printf("SQS message: %s", e.Body)
+
+	var snsEvent events.SNSEntity
+	if err := json.Unmarshal([]byte(e.Body), &snsEvent); err != nil {
 		return "", errors.Wrapf(err, "failed to unmarshal: %s", e.Body)
 	}
-	str, err := url.QueryUnescape(s3event.Records[0].S3.Object.Key)
+	log.Printf("SNS message: %s", snsEvent.Message)
+
+	var s3event events.S3Event
+	if !strings.Contains(snsEvent.Message, s3PutEvent) {
+		return "", nil
+	}
+	if err := json.Unmarshal([]byte(snsEvent.Message), &s3event); err != nil {
+		return "", errors.Wrapf(err, "failed to unmarshal: %s", e.Body)
+	}
+	key, err := url.QueryUnescape(s3event.Records[0].S3.Object.Key)
 	if err != nil {
 		return "", errors.Wrapf(err, "failed to unescape file name: %s", s3event.Records[0].S3.Object.Key)
 	}
-	return filepath.Ext(str), nil
+
+	return filepath.Base(key[:len(key)-len(filepath.Ext(key))]), nil
 }
